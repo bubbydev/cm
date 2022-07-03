43cefde95c0e0bc28bf46a2fac612ad5cb772017
Priyanka <priyanka.businesslabs@gmail.com>
2022-07-03 15:13:07 +0530

    Git Created

---
+++ resources/views/admin/login.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.admin')
+@section('content')
+<div class="login-box">
+  <div class="login-logo">
+    <a href="{{ url('/') }}"><b>Admin</b>LTE</a>
+  </div>
+  <!-- /.login-logo -->
+  <div class="login-box-body">
+    <p class="login-box-msg">Sign in to start your session</p>
+    <form action="{{ url('/admin/login') }}" method="post">
+      {{ csrf_field() }}
+      <div class="form-group has-feedback">
+        <input type="email" class="form-control" name="email" placeholder="Email">
+        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+      </div>
+      <div class="form-group has-feedback">
+        <input type="password" class="form-control" name="password" placeholder="Password">
+        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+      </div>
+      <div class="row">
+        <div class="col-xs-8">
+          <div class="checkbox icheck">
+            <label>
+              <input type="checkbox"> Remember Me
+            </label>
+          </div>
+        </div>
+        <!-- /.col -->
+        <div class="col-xs-4">
+          <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+        </div>
+        <!-- /.col -->
+      </div>
+    </form>
+    <a href="#">I forgot my password</a><br>
+    <a href="{{ url('/admin/register') }}" class="text-center">Register a new membership</a>
+  </div>
+  <!-- /.login-box-body -->
+  
+    
+@endsection
